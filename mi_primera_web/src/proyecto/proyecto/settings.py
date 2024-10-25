9d0ccffc663cbbcc3487a7f668909c64bdf51804
RickyVdev <rickyvdev18@gmail.com>
2024-10-25 16:47:06 -0600

    requirements.txt added

--- mi_primera_web/src/proyecto/proyecto/settings.py
+++ mi_primera_web/src/proyecto/proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['todo-tuvb.onrender.com']
+ALLOWED_HOSTS = ['todo-tuvb.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
